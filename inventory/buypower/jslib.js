afff779cbd0f7d7a011b8cbb3b5f8c5d3de2251d
Ivan Song <mousewurx@gmail.com>
2024-07-24 21:13:39 -0700

    first commit

---
+++ inventory/buypower/jslib.js
@@ -0,0 +1,105 @@
+function selectedTextOf(aSelectObj)
+{
+	var selectIdx=aSelectObj.selectedIndex
+	var selectStr
+	if (selectIdx<0)
+	{
+		selectStr=""
+	}
+	else
+	{
+		selectStr=(aSelectObj.options[selectIdx]).text
+	}
+	return selectStr
+}
+// Returns the selected value of a Select object.
+function selectedValue( aSelectObj )
+{
+    var selectIdx = aSelectObj.selectedIndex;
+    var selectStr = (aSelectObj.options[ selectIdx ]).value;
+    return selectStr;
+}
+
+// Opens popup window with trim overview page
+function trimOverview (pModlYrNbr, pBrand, pPVC)
+{
+// pModlYrNbr - The Model Year Number
+// pBrand     - Either a Brand Code, or a caret-delimited (^) string where
+//              Brand Code is the third element (as the Year, Make, Model, Trim
+//              pages will pass it).
+// pPVC       - Either a PVC Code or a caret-delimited (^) string where PVCId 
+//              is the first element (as the Year, Make, Model, Trim pages will
+//              pass it).
+// Can be called with:
+//    1) No parms
+//    2) pModlYrNbr and pBrand
+//    3) pModlYrNbr, a pBrand (which can be an empty string), and pPVC
+   var parmList = "?PopupIndctr=Y&OvrvwTypeId=T";
+   
+      // If ModlYrNbr is null, Brand and PVC must also be null and I need to do nothing. 
+      if (pModlYrNbr != null)
+      {
+         // If Model Year is empty, display error.
+         if (pModlYrNbr == "")
+         {
+            errorPopup ('Please select<br>a model<br>to continue.','select');
+            return;
+         }
+         else
+         {
+            // PVC is more specific. If have that, ignore Brand
+            if (pPVC == null || pPVC == "")
+            {
+               if (pBrand == null || pBrand == "")
+               {
+                  // Model Year has a value, but neither Brand or PVC do. display an error.
+                  errorPopup ('Please select<br>a model<br>to continue.','select');
+                  return;
+               }
+               else
+               {
+                  // Have a Brand and Model Year, but not a PVC. Send Model Year and Brand
+                  
+                  // Check if the pBrand parm is tokenized
+                  if (pBrand.indexOf("^") >= 0)
+                  {
+                     arrayBrand = pBrand.split("^");
+                     // BrandCd is the third element
+                     BrandCd = arrayBrand[2];
+                  }
+                  else
+                  {
+                     BrandCd = pBrand;
+                  }
+
+                  parmList = parmList+"&ModlYrNbr="+pModlYrNbr+"&BrandCd="+BrandCd;
+               }
+            }
+            else
+            {
+               // Have a Model Year and PVC. Send Model and PVC
+                  
+               // Check if the pPVC parm is tokenized
+               if (pPVC.indexOf("^") >= 0)
+               {
+                  arrayPVC = pPVC.split("^");
+                  // PVCId is the first element
+                  PVCId = arrayPVC[0];
+               }
+               else
+               {
+                  PVCId = pPVC;
+               }
+
+               parmList = parmList+"&ModlYrNbr="+pModlYrNbr+"&PVCId="+PVCId;
+            }
+         }
+      }
+               
+
+   linkTo='/cgi-bin/gx.cgi/AppLogic+COM.gm.BuyPower.applications.modelInfo.ViewModelDetailsOverview'+parmList;
+
+   // The Trim Overview returns its own frameset, so open an unframed popup window.
+   popupSingle (640,420,linkTo);
+}
+
---
+++ inventory/buypower/pulldowns.js
@@ -0,0 +1,95 @@
+function aM(year,make,ss,nmplt)
+{
+	if (v[year]==null)
+	{
+		v[year]=new Year(year)
+	}
+	v[year].makes[make]=new Make(make,ss,nmplt)
+}
+function aV(year,make,model,brandCd)
+{
+	var modelObj=new Model(model, v[year].makes[make].sellngSrcCd + '^' + v[year].makes[make].nampltCd + '^' + brandCd)
+	v[year].models[model]=modelObj
+	v[year].makes[make].models[model]=modelObj
+}
+function Year(yearStr)
+{
+	this.year=yearStr
+	this.models=new Array()
+	this.makes=new Array()
+}
+function Model(n,mab)
+{
+	this.modelName=n
+	this.makeAndBrand=mab
+}
+function Make(makeName,ss,nmplt)
+{
+	this.makeName=makeName
+	this.sellngSrcCd=ss
+	this.nampltCd=nmplt
+	this.models=new Array()
+	this.bodyStyles=new Array()
+}
+function buildModelMenu(yearStr,makeStr, selectedModelStr)
+{
+	document.SDForm.SelectModel.options.length=1
+	document.SDForm.SelectModel.options.selectedIndex=0
+	var allModels
+	if((makeStr=="")||document.SDForm.SelectMake.selectedIndex==0)
+	{
+		allModels=(v[yearStr]).models
+	}
+	else
+	{
+		makes=(v[yearStr]).makes
+		allModels=(makes[makeStr]).models
+	}
+	var i=1
+	for(aModelStr in allModels)
+	{
+		aModel=allModels[aModelStr]
+		document.SDForm.SelectModel.options[i]=new Option(aModel.modelName,aModel.makeAndBrand);
+		if (aModel.modelName == selectedModelStr)
+		{
+			document.SDForm.SelectModel.options.selectedIndex=i;
+		}
+		i++
+	}
+}
+function buildMakeMenu(yearStr, selectedMakeStr)
+{
+	document.SDForm.SelectMake.options.length=1
+	document.SDForm.SelectMake.options.selectedIndex=0
+	var makes=(v[yearStr]).makes
+	var i=1
+	for(aMakeIdx in makes)
+	{
+		var aMake=makes[aMakeIdx];
+		var ssNmpltCombo = aMake.sellngSrcCd + '^' + aMake.nampltCd;
+		document.SDForm.SelectMake.options[i]=new Option(aMake.makeName,ssNmpltCombo);
+if (aMake.makeName == selectedMakeStr)
+{
+	document.SDForm.SelectMake.options.selectedIndex=i;
+}
+		i++;
+	}
+}
+function selectMake(aSelect)
+{
+	selMakeStr=selectedTextOf(aSelect)
+	selYearStr=selectedTextOf(document.SDForm.ModlYrNbr)
+	buildModelMenu(selYearStr,selMakeStr)
+	document.SDForm.SelectTrim.options.length=1
+	document.SDForm.SelectTrim.options[0] = new Option("Which [Model Name] Style?","");
+	document.SDForm.SelectTrim.options.selectedIndex=0
+}
+function selectYear(selectObj)
+{
+	var yearStr=selectedTextOf(selectObj)
+	buildMakeMenu(yearStr,"")
+	buildModelMenu(yearStr,"","")
+	document.SDForm.SelectTrim.options.length=1
+	document.SDForm.SelectTrim.options[0] = new Option("Which [Model Name] Style?","");
+	document.SDForm.SelectTrim.options.selectedIndex=0
+}
